4b89793037b7e56e460357e97c24161553596399
Scott Westover <scottwestover2006@gmail.com>
2024-11-24 19:56:46 -0500

    added example project for phaser editor demo
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ hugo-site/static/games/phaser-editor-full-screen-demo/src/scenes/Level.js
@@ -0,0 +1,67 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import FullScreenButtonPrefab from "../FullScreenButtonPrefab.js";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Level extends Phaser.Scene {
+
+	constructor() {
+		super("Level");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @returns {void} */
+	editorCreate() {
+
+		// dino
+		const dino = this.add.image(640, 288, "dino");
+		dino.setInteractive(new Phaser.Geom.Rectangle(0, 0, 250, 250), Phaser.Geom.Rectangle.Contains);
+
+		// welcome
+		const welcome = this.add.text(640, 478, "", {});
+		welcome.setOrigin(0.5, 0.5);
+		welcome.text = "Phaser 3 + Phaser Editor v4";
+		welcome.setStyle({ "fontFamily": "Arial", "fontSize": "30px" });
+
+		// fullScreenButtonPrefab
+		const fullScreenButtonPrefab = new FullScreenButtonPrefab(this, 1228, 49);
+		this.add.existing(fullScreenButtonPrefab);
+
+		this.dino = dino;
+		this.welcome = welcome;
+
+		this.events.emit("scene-awake");
+	}
+
+	/** @type {Phaser.GameObjects.Image} */
+	dino;
+	/** @type {Phaser.GameObjects.Text} */
+	welcome;
+
+	/* START-USER-CODE */
+
+	// Write more your code here
+
+	create() {
+
+		this.editorCreate();
+
+		this.dino.on("pointerdown", () => {
+
+			this.welcome.text = "Hello, World!";
+		});
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
